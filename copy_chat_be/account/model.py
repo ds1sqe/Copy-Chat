3911c45ce062d7805cf95e350e898e5dd68332fb
ds1sqe <ds1sqe@mensakorea.org>
2023-03-14 22:43:07 +0900

    define core structures

---
+++ copy_chat_be/account/model.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import Permission, AbstractUser
+from django.db import models
+
+class Account(AbstractUser):
+    # profile
+    # relation
+    # state
+    # permissions
+    
+    # to repr
