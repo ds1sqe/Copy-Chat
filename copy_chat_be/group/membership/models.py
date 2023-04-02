265db284117fe27412f59ca229db4b8e1b2b9324
ds1sqe <ds1sqe@mensakorea.org>
2023-04-02 17:09:23 +0900

    add default membership

---
+++ copy_chat_be/group/membership/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from account.models import Account
+
+from group.models import Group
+
+class Permission:
+    """
+    1         1      => invite member
+    2        10      => kick member
+
+    4       100      => grant membership (give or take membership)
+    8      1000      => change membership (permission, name)
+
+    16    10000      => delete group
+    32   100000      => modify group
+
+    """
+    
+    def __init__(self,permission):
+        self.invite_member = (1&permission)>0
+        self.kick_member= (2&permission)>0
+
+        self.grant_membership=(4&permission)>0
+        self.change_mambership=(8&permission)>0
+
+        self.delete_group = (16&permission)>0
+        self.modify_group = (32&permission)>0
+
+    def all():
+        return 63
+
+    def invite_only():
+        return 1
+
+
+class GroupMembership(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE,related_name="membership")
+    name = models.CharField(max_length=20)
+
+    owners = models.ManyToManyField(Account)
+
+    is_default = models.BooleanField(default=False)
+    permission = models.IntegerField(default=0)
+
+    class Meta:
+        unique_together = [["group", "name"],["group","is_default"]]
---
+++ copy_chat_be/group/views.py
@@ -0,0 +1,97 @@
+from account.models import Account
+from django.http import JsonResponse
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+
+from .models import Group
+from .serializers import GroupSerializer
+
+from .membership.models import GroupMembership, Permission
+
+
+class GroupView(generics.ListAPIView,generics.CreateAPIView,generics.DestroyAPIView):
+    
+    http_method_names = ["get","post", "delete"]
+    permission_classes = (IsAuthenticated,)
+    serializer_class = GroupSerializer
+
+    def get(self,request:Request,*args,**kwargs):
+        """
+        get groups which accounts' owner have enlisted
+        """
+        groups = request.user.joined_groups.all()
+        if len(groups)==0:
+            return JsonResponse(status=status.HTTP_204_NO_CONTENT,data={"group":None},safe=False)
+        else:
+            s = self.get_serializer(groups, many=True)
+            return JsonResponse(data={"group":s.data},safe=False)
+
+
+    def post(self, request: Request, *args, **kwargs):
+        """
+        Create new group
+        """
+        group_name= request.data.get("groupname")
+
+        if not group_name:
+            return JsonResponse(
+                    status=status.HTTP_400_BAD_REQUEST,data={"success":False,"msg":"please provide group name"}
+                    ,safe= False
+                )
+
+        group_exists= Group.objects.filter(name=group_name).exists()
+
+        if group_exists:
+            return JsonResponse(
+                status=status.HTTP_409_CONFLICT,data={"success": False, "msg": "group_name duplicated"}, safe=False
+            )
+
+        else:
+                s :GroupSerializer = self.get_serializer(data={"name":group_name},context=request)
+                if s.is_valid():
+                    group = s.save()
+                    group.save()
+
+                    group.members.add(request.user)
+
+                    membership = GroupMembership.objects.create(
+                    group=group,name="Head",
+                    permission=Permission.all()
+                    )
+
+                    membership.owners.add(request.user)
+
+                    GroupMembership.objects.create(
+                    group=group,name="Member",
+                    is_default=True,
+                    permission=Permission.invite_only()
+                    )
+
+                    return JsonResponse(
+                            status=status.HTTP_201_CREATED,data={"success":True,
+                                  "msg":"group created","group":GroupSerializer(group).data}, safe=False
+                    )
+                return JsonResponse(
+                    {"success": False, "msg": "invaild request"}, safe=False
+                )
+
+
+    def delete(self, request: Request, *args,**kwargs):
+        group_id = request.data.get("group_id")
+        try:
+            group = Group.objects.get(id=group_id)
+        except Group.DoesNotExist as e :
+            return JsonResponse(data={"success":False,
+                                      "msg":"Need Permission to Delete Group","detail":str(e)},safe=False)
+        
+        try:
+            membership = GroupMembership.objects.filter(group=group,owners__in=[request.user]).get()
+            p = Permission(membership.permission)
+            if p.delete_group:
+                group.delete()
+                return JsonResponse(data={"success":True,
+                                          "group_id":group_id,},safe=False)
+        except GroupMembership.DoesNotExist as e :
+            return JsonResponse(data={"success":False,
+                                      "msg":"Need Permission to Delete Group","detail":str(e)},safe=False)
